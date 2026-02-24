fa80e8e34dc5b0c84a6f6158059555050141ca3d
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-02-24 19:23:50 +0700

    mengubah dari teks ke sesuai tugas

--- try1.cpp
+++ try1.cpp
@@ -5,17 +5,7 @@ int main() {
     int umur = 15;
     umur = 20;
 
-    cout << "Umur saya adalah usia saya: " << umur << endl;
+    cout << "ini string" << endl;
     cout << 123 << endl;
-
-    cout << "Hello World!" << endl;
-    cout << "Saya sedang belajar C++" << endl;
-    cout << "C++ adalah bahasa pemrograman yang powerful!" << endl;
-    cout << "Saya sangat menikmati belajar C++!" << endl;
-    cout << "C++ memungkinkan saya untuk membuat berbagai jenis aplikasi!" << endl;
-    cout << "Saya ingin menjadi seorang programmer C++ yang handal!" << endl;
-    cout << "Terima kasih telah membaca pesan saya!" << endl;
-    cout << "Semoga hari Anda menyenangkan!" << endl;
-    cout << "Sampai jumpa lagi!" << endl;
-    cout << "Selamat belajar C++!" << endl;
+    cout << umur;
 }
