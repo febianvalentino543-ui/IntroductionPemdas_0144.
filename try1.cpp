5597073e3531d97d8e0cbfed6715e35ad1df86e5
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-02-24 19:05:12 +0700

    menambahkan review

--- try1.cpp
+++ try1.cpp
@@ -6,4 +6,9 @@ int main() {
     umur = 20;
 
     cout << "Umur saya adalah usia saya: " << umur << endl;
+    cout << 123 << endl;
+
+    cout << "Hello World!" << endl;
+    cout << "Saya sedang belajar C++" << endl;
+    cout << "C++ adalah bahasa pemrograman yang powerful!" << endl;
 }
